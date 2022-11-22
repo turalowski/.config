35f315a0eb75a692df24ca08d3973d0600f2a7f7
Tural Hajiyev <38193384+turalowski@users.noreply.github.com>
2022-11-22 14:37:07 +0100

    Update keymaps.lua

--- nvim/lua/turalowski/core/keymaps.lua
+++ nvim/lua/turalowski/core/keymaps.lua
@@ -43,6 +43,21 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
+
+-- lsp key bindings
+--   keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)
+--   keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+--   keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+--   keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+--   keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+--   keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
+--   keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+--   keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+--   keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+--   keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+--   keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+--   keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
+
 -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
